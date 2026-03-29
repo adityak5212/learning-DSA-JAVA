b70198536bb8a67d7e90f617dd1280b54d07f91b
Aditya Gupta <adityak5212@gmail.com>
2026-03-29 16:16:21 +0530

    ArrayList and LinkedList

---
+++ main.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class main{
+    public static void main(String[] args){
+
+        LinkedList<Integer> linkedList = new LinkedList<Integer>();
+        ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+        long startTime;
+        long endTime;
+        long elapsedTime;
+
+        for(int i = 0; i < 1000000; i++){
+            linkedList.add(i);
+            arrayList.add(i);
+        }
+        // ***************LinkedList********************
+        startTime = System.nanoTime();
+
+        linkedList.get(0);
+
+        endTime =System.nanoTime();
+
+        elapsedTime = endTime - startTime;
+
+        System.out.println("LinkedList:\t" + elapsedTime +"ns");
+
+        // *****************ArrayList***********************
+
+        startTime = System.nanoTime();
+
+        arrayList.get(0);
+
+        endTime =System.nanoTime();
+
+        elapsedTime = endTime - startTime;
+
+        System.out.println("ArrayList:\t" + elapsedTime +"ns");
+
+
+    }
+}
